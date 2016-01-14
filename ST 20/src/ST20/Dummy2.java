ebe5f6bc926dc55da39fe97b46475a2a59c1653a
p.merkel <p.merkel@141.19.145.118>
2016-01-14 11:06:48 +0100

    Changed: Dummy2.java, erste Anweisung verändert, Task ST 20, Dustin Noah - Paul

--- ST 20/src/ST20/Dummy2.java
+++ ST 20/src/ST20/Dummy2.java
@@ -3,7 +3,7 @@ package ST20;
 public class Dummy2 {
 	public void match(int a, int b){
 		if(a > b){
-			System.out.println("a ist groesser als b!");
+			System.out.println("a ist groesser als b! Wieso tun wir uns das eigentlich an?");
 			System.out.println("Und es ist uns total Wayne -.- !");
 		}
 	}
