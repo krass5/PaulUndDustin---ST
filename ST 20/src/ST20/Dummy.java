90135d2e79208b42a5cae5a372cd89aa96210001
p.merkel <p.merkel@141.19.145.118>
2016-01-14 10:53:31 +0100

    Added: Dummy.java, Methode mit drei Anweisungen, Task ST 20, Dustin Noah - Paul

---
+++ ST 20/src/ST20/Dummy.java
@@ -0,0 +1,13 @@
+package ST20;
+
+public class Dummy {
+	public void match(int a, int b){
+		if(a > b){
+			System.out.println("a ist groesser als b!");
+		}else if(a < b){
+			System.out.println("a ist kleiner als b!");
+		}else if(a == b){
+			System.out.println("a und b sind gleich!");
+		}
+	}
+}
