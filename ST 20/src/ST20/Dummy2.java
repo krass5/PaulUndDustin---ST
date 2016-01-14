d0fd022183fc741bd1cb782271780174a5e58cff
p.merkel <p.merkel@141.19.145.118>
2016-01-14 11:04:40 +0100

    Added: Dummy2.java, Methode mit zwei aufeinanderfolgenden Anweisungen, Task ST 20, Dustin Noah - Paul

---
+++ ST 20/src/ST20/Dummy2.java
@@ -0,0 +1,10 @@
+package ST20;
+
+public class Dummy2 {
+	public void match(int a, int b){
+		if(a > b){
+			System.out.println("a ist groesser als b!");
+			System.out.println("Und es ist uns total Wayne -.- !");
+		}
+	}
+}
